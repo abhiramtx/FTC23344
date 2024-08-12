40e7f9f8c0e20bd2d7177b678db95d9df192c221
Air_heads <106994329+abhiramtx@users.noreply.github.com>
2024-08-11 19:36:36 -0500

    Added new website files

---
+++ website/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/contributors')
+def contributors():
+    return render_template('contributors.html')
+
+@app.route('/robot')
+def robot():
+    return render_template('robot.html')
+
+@app.route('/competitions')
+def competitions():
+    return render_template('competitions.html')
+
+@app.route('/awards')
+def awards():
+    return render_template('awards.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/sponsors')
+def sponsors():
+    return render_template('sponsors.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
